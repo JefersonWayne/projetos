91edd05f843500dae7143055281c0da6fb608700
Flávio Jeferson <133133831+flaviojeferson@users.noreply.github.com>
2023-07-04 13:48:01 -0300

    feat: Implement mobile responsiveness
    Completed the script.js file

--- age-calculator/scripts/script.js
+++ age-calculator/scripts/script.js
@@ -1,72 +1,124 @@
 function calculateAge() {
-  const resetFields = document.querySelectorAll(".fieldErrorText");
-  resetFields.forEach(element => {
-    element.classList.remove("fieldErrorText");
-    if (element.tagName.toLowerCase() === "p") {
-      element.parentNode.removeChild(element);
-    }
-  });
+	const resetFields = document.querySelectorAll(".fieldErrorText");
+	resetFields.forEach(element => {
+		element.classList.remove("fieldErrorText");
+		if (element.tagName.toLowerCase() === "p") {
+			element.parentNode.removeChild(element);
+		}
+	});
 
-  const getInputValue = (element) => Number(element.value);
+	const getInputValue = (element) => Number(element.value);
 
-  const showError = (input, errorMessage) => {
-    const fieldError = document.createElement("p");
-    fieldError.textContent = errorMessage;
-    input.parentNode.appendChild(fieldError);
-    input.parentNode.querySelector("label").classList.add("fieldErrorText");
-    input.classList.add("fieldErrorText");
-    fieldError.classList.add("fieldErrorText");
-  };
+	const showError = (input, errorMessage) => {
+		const fieldError = document.createElement("p");
+		fieldError.textContent = errorMessage;
+		fieldError.classList.add("fieldErrorText");
+		input.parentNode.appendChild(fieldError);
+		input.classList.add("fieldErrorText");
+		input.setAttribute("aria-describedby", `${input.id}-error`);
+		input.setAttribute("aria-invalid", "true");
+		input.parentNode.querySelector("label").classList.add("fieldErrorText");
+	};
 
-  const divDay = document.querySelector("#divDay");
-  const divMonth = document.querySelector("#divMonth");
-  const divYear = document.querySelector("#divYear");
-  const dayInput = divDay.querySelector("#inputDay");
-  const monthInput = divMonth.querySelector("#inputMonth");
-  const yearInput = divYear.querySelector("#inputYear");
-  const dayValue = getInputValue(dayInput);
-  const monthValue = getInputValue(monthInput);
-  const yearValue = getInputValue(yearInput);
+	const removeError = (input) => {
+		const fieldError = input.parentNode.querySelector(`#${input.id}-error`);
+		if (fieldError) {
+			fieldError.parentNode.removeChild(fieldError);
+		}
+		input.classList.remove("fieldErrorText");
+		input.removeAttribute("aria-describedby");
+		input.removeAttribute("aria-invalid");
+		input.parentNode.querySelector("label").classList.remove("fieldErrorText");
+	};
 
-  const daysRange = 30.4;
+	const divDay = document.querySelector("#divDay");
+	const divMonth = document.querySelector("#divMonth");
+	const divYear = document.querySelector("#divYear");
+	const dayInput = divDay.querySelector("#inputDay");
+	const monthInput = divMonth.querySelector("#inputMonth");
+	const yearInput = divYear.querySelector("#inputYear");
+	const dayValue = getInputValue(dayInput);
+	const monthValue = getInputValue(monthInput);
+	const yearValue = getInputValue(yearInput);
 
-  const validateInput = (value, input, errorMessage) => {
-    if (value <= 0 || input.value.length <= 0 || value > daysRange) {
-      showError(input, errorMessage);
-      return false;
-    }
-    return true;
-  };
+	const validateInput = (value, input, errorMessage) => {
+		if (isNaN(value) || value <= 0 || value > 31) {
+			showError(input, errorMessage);
+			return false;
+		}
+		removeError(input);
+		return true;
+	};
 
-  const allInputsValid = () => {
-    let isValid = true;
-    isValid = validateInput(dayValue, dayInput, "Must be a valid day") && isValid;
-    isValid = validateInput(monthValue, monthInput, "Must be a valid month") && isValid;
-    isValid = yearValue > 0 && yearInput.value.length > 0 && isValid;
-    if (!isValid) {
-      showError(yearInput, "This field is required");
-    }
-    return isValid;
-  };
+	const validateYearInput = (value, input) => {
+		if (value === "") {
+			showError(input, "This field is required");
+			return false;
+		}
+		if (isNaN(value) || value <= 0 || input.value.length !== 4) {
+			showError(input, "Invalid year");
+			return false;
+		}
+		removeError(input);
+		return true;
+	};
 
-  if (!allInputsValid()) {
-    return;
-  }
+	const allInputsValid = () => {
+		let isValid = true;
+		isValid = validateInput(dayValue, dayInput, "Invalid day") && isValid;
+		isValid = validateInput(monthValue, monthInput, "Invalid month") && isValid;
+		isValid = validateYearInput(yearValue, yearInput) && isValid;
+		return isValid;
+	};
 
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentDay = currentDate.getDate();
-  const inputYear = getInputValue(yearInput);
-  const inputMonth = getInputValue(monthInput);
-  const inputDay = getInputValue(dayInput);
-  const totalDays = (currentYear - inputYear) * 365 + (currentMonth - inputMonth) * 30 + (currentDay - inputDay);
+	if (!allInputsValid()) {
+		return;
+	}
 
-  const years = Math.floor(totalDays / 365);
-  const months = Math.floor((totalDays % 365) / 30);
-  const days = Math.floor((totalDays % 365) % 30);
+	const currentDate = new Date();
+	const currentYear = currentDate.getFullYear();
+	const currentMonth = currentDate.getMonth() + 1;
+	const currentDay = currentDate.getDate();
+	const inputYear = getInputValue(yearInput);
+	const inputMonth = getInputValue(monthInput);
+	const inputDay = getInputValue(dayInput);
 
-  document.getElementById('years-value').textContent = years;
-  document.getElementById('months-value').textContent = months;
-  document.getElementById('days-value').textContent = days;
+	// Validate if the entered year is greater than the current year
+	if (inputYear > currentYear) {
+		removeError(yearInput);
+		showError(yearInput, "Entered year is in the future");
+		return;
+	}
+
+	// Validate if the entered month is within the valid range (1-12)
+	if (inputMonth <= 0 || inputMonth > 12) {
+		removeError(monthInput);
+		showError(monthInput, "Invalid month");
+		return;
+	}
+
+	// Convert the entered dates to Date objects
+	const enteredDate = new Date(inputYear, inputMonth - 1, inputDay);
+	const currentDateObj = new Date(currentYear, currentMonth - 1, currentDay);
+
+	// Compare the timestamps of the entered date and current date
+	if (enteredDate.getTime() > currentDateObj.getTime()) {
+		// Dates are valid
+		removeError(yearInput);
+		showError(yearInput, "Entered date is in the future");
+		return;
+	}
+
+	const totalDays =
+		(currentYear - inputYear) * 365 +
+		(currentMonth - inputMonth) * 30.4 +
+		(currentDay - inputDay);
+
+	const years = Math.floor(totalDays / 365);
+	const months = Math.floor((totalDays % 365) / 30.4);
+	const days = Math.floor((totalDays % 365) % 30.4);
+
+	document.getElementById("years-value").textContent = years;
+	document.getElementById("months-value").textContent = months;
+	document.getElementById("days-value").textContent = days;
 }
